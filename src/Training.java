4269f39f0cc264c060325ddd69b72827b79a6704
Zongpol Jongkirkkiat <levatinez@gmail.com>
2018-11-24 14:07:06 +0700

    Just Checking

--- src/Training.java
+++ src/Training.java
@@ -1,4 +1,9 @@
-
 public class Training {
-
+	public Training() {
+		int win = 0;
+		//System.out.println("kuy win");
+	}
+	public static void main(String [] args) {
+		System.out.println("XXX");
+	}
 }
