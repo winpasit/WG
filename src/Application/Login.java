ac47b51a051e0d9db5f1446eb6bcf7a909d7a972
Pasit Laohawarutchai <winywinywiny@hotmail.com>
2018-11-24 17:23:15 +0700

    login

---
+++ src/Application/Login.java
@@ -0,0 +1,16 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
