8e37d30983d7c8856ef57a41e67272150a083d45
Pasit Laohawarutchai <winywinywiny@hotmail.com>
2018-11-24 15:01:18 +0700

    Test

--- src/Training.java
+++ src/Training.java
@@ -1,9 +1,5 @@
 public class Training {
 	public Training() {
-		int win = 0;
-		//System.out.println("kuy win");
-	}
-	public static void main(String [] args) {
-		System.out.println("XXX");
+		System.out.println("Accountant");
 	}
 }
