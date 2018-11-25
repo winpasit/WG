27da67ac4fd505bb6790dc73818fca8f35b40a8e
Zongpol Jongkirkkiat <levatinez@gmail.com>
2018-11-25 13:58:00 +0700

    update bodyChoose1

--- src/Application/BodyChoose.java
+++ src/Application/BodyChoose.java
@@ -1,6 +1,10 @@
 package Application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -13,7 +17,23 @@ public class BodyChoose extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		 HBox hbox  = new HBox();
+		 hbox.setAlignment(Pos.CENTER);
+		 //hbox.setHgap(10);
+		 //hbox.setVgap(10);
+		 hbox.setPadding(new Insets(25, 25, 25, 25));
+		 
+		 /*TextField weightTextField = new TextField();
+		 weightTextField.setPromptText("your weight");
+		 hbox.getChildren().add(weightTextField);*/
+		 
+		 Scene scene = new Scene(hbox, 350, 300);
+
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("BodyChoose");
+			primaryStage.show();
 	}
+	
+	
 	
 	public BodyChoose() {
 
