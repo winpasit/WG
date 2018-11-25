ae5336e14c25ff1a78f393372085d7bfcf09f2e3
Pasit Laohawarutchai <winywinywiny@hotmail.com>
2018-11-25 11:54:54 +0700

    Add login page (can change design later) and must change action to link with another page

--- src/Application/Login.java
+++ src/Application/Login.java
@@ -1,15 +1,135 @@
 package Application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Login extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		Text scenetitle = new Text("Welcome to Workout Generator");
+		grid.add(scenetitle, 0, 0, 2, 1);
+
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
+
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1);
+
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
+
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		Button signinBtn = new Button("Sign in");
+		signinBtn.setPrefWidth(65);
+		Button exitBtn = new Button("Exit");
+		exitBtn.setPrefWidth(65);
+		hbBtn.getChildren().addAll(signinBtn, exitBtn);
+		grid.add(hbBtn, 1, 4);
+
+		// set style
+		userName.setStyle("-fx-text-fill: white;");
+		pw.setStyle("-fx-text-fill: white;");
+		grid.setStyle("-fx-background-color:black;");
+		scenetitle.setStyle("-fx-font-size: 16px; -fx-font-family:\"Arial Black\";-fx-fill: lime;");
+		signinBtn.setStyle(
+				"-fx-text-fill: green; -fx-font-weight: bold; -fx-font-family: \"Arial Narrow\"; -fx-background-color: white;");
+		exitBtn.setStyle(
+				"-fx-text-fill: crimson; -fx-font-weight: bold; -fx-font-family: \"Arial Narrow\"; -fx-background-color: white;");
+
+		// add handler
+		// press ESC to clear user text field
+		userTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent ke) {
+				if (ke.getCode() == KeyCode.ESCAPE) {
+					userTextField.setText("");
+				}
+			}
+		});
+		// change button width when mouse over
+		signinBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				signinBtn.setPrefWidth(75);
+			}
+		});
+		signinBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				signinBtn.setPrefWidth(65);
+			}
+		});
+		exitBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				exitBtn.setPrefWidth(75);
+			}
+		});
+		exitBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				exitBtn.setPrefWidth(65);
+			}
+		});
+		// action when click button
+		signinBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Welcome Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText("Welcome, "+userTextField.getText()+". Your Password is "+pwBox.getText()+".");
+				alert.showAndWait();
+				
+			}
+		});
+		exitBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				primaryStage.close();
+			}
+		});
+
+		Scene scene = new Scene(grid, 350, 300);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("JavaFX Welcome");
+		primaryStage.show();
 	}
 
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
