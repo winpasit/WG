4545bdb0ed6f5e348f0a945c05dbaaf2e8e41e5b
Zongpol Jongkirkkiat <levatinez@gmail.com>
2018-11-27 14:30:15 +0700

    cannot access bodychoose page if invalid weight or days per week

--- src/Application/UserInput.java
+++ src/Application/UserInput.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -22,28 +24,55 @@ public class UserInput extends Application {
 		gp.setVgap(10);
 		gp.setPadding(new Insets(25, 25, 25, 25));
 
-		Label weight = new Label("Weight : ");
+		Label weight = new Label("Weight(kg) : ");
 		gp.add(weight, 0, 0);
 		TextField weightTextField = new TextField();
 
 		weightTextField.setPromptText("your weight");
 		gp.add(weightTextField, 1, 0);
 
-		Label dayPerWeek = new Label("Day per week : ");
+		Label dayPerWeek = new Label("day per week : ");
 		gp.add(dayPerWeek, 0, 1);
 		TextField dayTextField = new TextField();
-		dayTextField.setPromptText("the number of day");
+		dayTextField.setPromptText("the number of day(s) active");
 		gp.add(dayTextField, 1, 1);
+		
 
 		Button btn = new Button("Next");
 		// add action of this button to go to BodyChoose page
 		btn.setOnAction(e -> {
+			if (weightTextField.getText().equals("") || dayTextField.getText().equals("")) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Error Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText(
+						"Please fill in both weight and days per week");
+				alert.showAndWait();
+			}
+			else if (Integer.parseInt(dayTextField.getText()) <= 0 || Integer.parseInt(dayTextField.getText()) > 7 ) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Error Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText(
+						"Invalid day(s) per week");
+				alert.showAndWait();
+			}
+			else if (Integer.parseInt(weightTextField.getText()) <= 0) {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Error Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText(
+						"Please fill in reasonable weight");
+				alert.showAndWait();
+			}
+			else {
 			BodyChoose body = new BodyChoose();
 			primaryStage.close();
 			body.start(primaryStage);
+			}
 		});
 		gp.add(btn, 1, 2);
-		gp.setStyle("-fx-background-color:#CCFF99;");
+		gp.setStyle("-fx-background-color:beige;");
 
 		// change style here
 
