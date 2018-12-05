7261ff40fbc794f78e5ef6b046d6e69a45521761
Pasit Laohawarutchai <winywinywiny@hotmail.com>
2018-12-05 10:01:00 +0700

    comment tableview , add table that build by pane

--- src/Application/Schedule.java
+++ src/Application/Schedule.java
@@ -29,7 +29,7 @@ public class Schedule extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		TableView table = new TableView();
+		/*TableView table = new TableView();
 
 		Scene scene = new Scene(new Group());
 		primaryStage.setTitle("Table View Sample");
@@ -67,13 +67,9 @@ public class Schedule extends Application {
 		vbox.setSpacing(5);
 		vbox.setPadding(new Insets(10, 0, 0, 10));
 		vbox.getChildren().addAll(hbox, table);
-
-		((Group) scene.getRoot()).getChildren().addAll(vbox);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
 		
-		
+		VBox monday = new VBox();
+		monday.setSpacing(2);
 		VBox tuesday = new VBox();
 		tuesday.setSpacing(2);
 		VBox wednesday = new VBox();
@@ -87,15 +83,43 @@ public class Schedule extends Application {
 		VBox sunday = new VBox();
 		sunday.setSpacing(2);
 		
-		if(us.getBodyShape().equals("Ectomorph")) {
-			Calisthenics cali = new Calisthenics();
-			VBox monday = new VBox();
-			monday.setSpacing(2);
-			Button mon = new Button(cali.getDayExercise(1).getMoveName(0));
-			monday.getChildren().addAll(mon);
-			
+		//if(us.getBodyShape().equals("Ectomorph")) {
+		//	Calisthenics cali = new Calisthenics();
+		//	for(int i = 0 ; i < cali.getWeekExerciseList().size() ; i++) {
+		//		for(int j = 0 ; j < cali.getDayExercise(i).getMoveList().size(); j++) {
+		//			if(i == 1) {
+		//				Button mon = new Button(cali.getDayExercise(i).getMoveName(j));
+		//				monday.getChildren().addAll(mon);
+		//			}
+		//		}
+		//		table.getItems().add(monday);
+		//	}
 		}
-		//for (int i = 0; i < )
+
+		((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		*/
+		
+		
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(30, 30, 30, 30));
+		grid.setVgap(20);
+		//grid.setHgap(40);
+		grid.setAlignment(Pos.TOP_LEFT);
+		
+		for(int i = 1 ; i < 8 ; i++) {
+			Label lb = new Label("  Day "+ Integer.toString(i)+"  ");
+			lb.setStyle( "-fx-border-width: 2;" + "-fx-border-color: black;" + "-fx-font-size: 30");
+			grid.add(lb,i-1,0);
+		}
+		
+		
+		Scene scene = new Scene(grid, 860, 800);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Schedule");
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
